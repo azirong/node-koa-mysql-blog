14f4a9365599497e8bd64ff6496aaef9b990bf67
gaojiwei <gaojiwei@yidian-inc.com>
2018-03-08 11:10:33 +0800

    a new project

---
+++ config/default.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const config = {
+    // 启动端口
+    port: 3000,
+
+    // 数据库配置
+    database: {
+        DTATBASE: 'nodesql',
+        USERNAME: 'root',
+        PASSWORD: '111111',
+        PORT: '3306',
+        HOST: 'localhost'
+    }
+}
+
+module.exports = config
